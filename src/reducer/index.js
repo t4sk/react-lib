e60170d956b5b7fa053d96e767d41f3f328fee2c
t4sk <tsk.nakamura@gmail.com>
2019-06-25 14:42:18 +0900

    data reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,18 +1,22 @@
 import { combineReducers } from "redux"
 import * as auth from "./auth"
 import * as queries from "./queries"
+import * as data from "./data"
 
 export const reducer = combineReducers({
-  queries: queries.reducer,
   auth: auth.reducer,
+  queries: queries.reducer,
+  data: data.reducer,
 })
 
 export const actions = {
-  queries: queries.actions,
   auth: auth.actions,
+  queries: queries.actions,
+  data: data.actions,
 }
 
 export const selectors = {
-  queries: queries.selectors,
   auth: auth.selectors,
+  queries: queries.selectors,
+  data: data.selectors,
 }
