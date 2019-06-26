8402125b0c8139186298136ab5eac7dac195b99d
t4sk <tsk.nakamura@gmail.com>
2019-06-26 11:53:20 +0900

    toast

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react"
+
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
 import logo from "./logo.svg"
 import "./App.css"
 
 export function App() {
+  toast("Wow so easy !")
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +25,8 @@ export function App() {
           Learn React
         </a>
       </header>
+
+      <ToastContainer />
     </div>
   )
 }
