c102094e23d71e29a6adcabd66b601fc21856c20
t4sk <tsk.nakamura@gmail.com>
2019-09-11 10:24:40 +0900

    transducer util

--- src/util/index.js
+++ src/util/index.js
@@ -32,3 +32,27 @@ export function get(val, keys, defaultVal) {
 
   return val
 }
+
+// transducer
+export function append(arr, x) {
+  arr.push(x)
+  return arr
+}
+
+export function map(f) {
+  return combine => (res, x) => combine(res, f(x))
+}
+
+export function filter(f) {
+  return combine => (res, x) => {
+    if (f(x)) {
+      return combine(res, x)
+    }
+
+    return res
+  }
+}
+
+export function transduce(transform, combine, init, coll) {
+  return coll.reduce(transform(combine), init)
+}
--- src/util/index.test.js
+++ src/util/index.test.js
@@ -1,4 +1,4 @@
-import { compose, chunk, get } from "./util"
+import { compose, chunk, get, transduce, map, filter, append } from "./util"
 
 test("compose", () => {
   expect(
@@ -24,5 +24,40 @@ describe("get", () => {
 
   test("default", () => {
     expect(get({}, ["a", "b", 0], 1)).toEqual(1)
+  })
+})
+
+describe("transduce", () => {
+  const arr = [1, 2, 3, 4, 5]
+
+  test("map to array", () => {
+    const res = transduce(
+      compose(
+        map(x => x + 1),
+        filter(x => x % 2 == 0)
+      ),
+      append,
+      [],
+      arr
+    )
+
+    expect(res).toEqual([2, 4, 6])
+  })
+  test("sum", () => {
+    function add(sum, x) {
+      return sum + x
+    }
+
+    const res = transduce(
+      compose(
+        map(x => x + 1),
+        filter(x => x % 2 == 0)
+      ),
+      add,
+      0,
+      arr
+    )
+
+    expect(res).toEqual(12)
   })
 })
