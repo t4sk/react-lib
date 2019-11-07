6ded86f7fb97add7567dbdaabe3f94fedc4b4241
t4sk <tsk.nakamura@gmail.com>
2019-11-07 15:09:54 +0900

    form

--- src/lib/form.js
+++ src/lib/form.js
@@ -1,5 +1,40 @@
+export function notEmpty(str) {
+  return !!str.trim()
+}
+
 export function isInteger(x) {
   const int = parseInt(x)
 
   return !isNaN(int) && int.toString() == x.toString()
+}
+
+export function parse(schema, inputs) {
+  return Object.entries(inputs).reduce((_inputs, [key, val]) => {
+    _inputs[key] = schema[key].parse(val)
+
+    return _inputs
+  }, {})
+}
+
+export function validate(schema, inputs) {
+  return Object.entries(schema).reduce((errors, [key, field]) => {
+    const val = inputs[key]
+
+    const _errors = field.validations
+      .map(({ validate, getErrorMessage }) => {
+        if (validate(val)) {
+          return
+        }
+
+        return getErrorMessage(val)
+      })
+      .filter(error => !!error)
+
+    if (_errors.length > 0) {
+      // return only the first error
+      errors[key] = _errors[0]
+    }
+
+    return errors
+  }, {})
 }
--- src/lib/form.test.js
+++ src/lib/form.test.js
@@ -1,4 +1,14 @@
-import { isInteger } from "./form"
+import { parse, validate, notEmpty, isInteger } from "./form"
+
+describe("notEmpty", () => {
+  test("valid", () => {
+    expect(notEmpty("123")).toEqual(true)
+  })
+
+  test("invalid", () => {
+    expect(isInteger("   ")).toEqual(false)
+  })
+})
 
 describe("isInteger", () => {
   test("valid", () => {
@@ -8,5 +18,77 @@ describe("isInteger", () => {
   test("invalid", () => {
     expect(isInteger("123.123")).toEqual(false)
     expect(isInteger(NaN)).toEqual(false)
+  })
+})
+
+const SCHEMA = {
+  str: {
+    parse: val => val.trim(),
+    validations: [
+      {
+        validate: notEmpty,
+        getErrorMessage: value => "Cannot be blank",
+      },
+    ],
+  },
+  num: {
+    parse: parseInt,
+    validations: [
+      {
+        validate: isInteger,
+        getErrorMessage: value => "Invalid number",
+      },
+      {
+        validate: value => value > 0,
+        getErrorMessage: value => "Must be greater than 0",
+      },
+    ],
+  },
+}
+
+const INPUTS = {
+  str: "  abc ",
+  num: "1",
+}
+
+test("parse", () => {
+  expect(parse(SCHEMA, INPUTS)).toEqual({
+    str: "abc",
+    num: 1,
+  })
+})
+
+describe("validate", () => {
+  test("valid", () => {
+    expect(validate(SCHEMA, INPUTS)).toEqual({})
+  })
+
+  test("invalid", () => {
+    expect(
+      validate(SCHEMA, {
+        ...INPUTS,
+        str: "   ",
+      })
+    ).toEqual({
+      str: "Cannot be blank",
+    })
+
+    expect(
+      validate(SCHEMA, {
+        ...INPUTS,
+        num: "",
+      })
+    ).toEqual({
+      num: "Invalid number",
+    })
+
+    expect(
+      validate(SCHEMA, {
+        ...INPUTS,
+        num: "0",
+      })
+    ).toEqual({
+      num: "Must be greater than 0",
+    })
   })
 })
