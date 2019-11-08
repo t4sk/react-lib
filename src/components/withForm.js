549da38f9e7ce6e28765018e3068ccf70bb958ce
t4sk <tsk.nakamura@gmail.com>
2019-11-08 11:55:48 +0900

    withForm

---
+++ src/components/withForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import * as form from "../lib/form"
+
+export default function withForm(schema, initialInputs) {
+  return Component => {
+    function Form(props) {
+      const { error, submitting } = props
+
+      const [inputs, setInput] = useState(initialInputs)
+      const [errors, setErrors] = useState({})
+
+      function onChange(name, value) {
+        setInput({
+          ...inputs,
+          [name]: value,
+        })
+      }
+
+      function onSubmit() {
+        if (submitting) {
+          return
+        }
+
+        setErrors({})
+
+        const _inputs = form.parse(schema, inputs)
+        const _errors = form.validate(schema, _inputs)
+
+        if (Object.keys(_errors).length > 0) {
+          setErrors(_errors)
+          return
+        }
+
+        props.onSubmit(_inputs)
+      }
+
+      return (
+        <Component
+          inputs={inputs}
+          errors={errors}
+          valid={!error && Object.keys(errors).length == 0}
+          onChange={onChange}
+          {...props}
+          onSubmit={onSubmit}
+        />
+      )
+    }
+
+    Form.propTypes = {
+      submitting: PropTypes.bool.isRequired,
+      error: PropTypes.string.isRequired,
+      onSubmit: PropTypes.func.isRequired,
+    }
+
+    return Form
+  }
+}
