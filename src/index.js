5d2669e19a5d9665b518a11fec9b2a70efc29196
t4sk <tsk.nakamura@gmail.com>
2019-11-08 12:14:59 +0900

    scroll to top

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "semantic-ui-css/semantic.min.css"
 import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 // redux
 import { applyMiddleware, createStore } from "redux"
@@ -20,11 +20,18 @@ if (process.env.NODE_ENV !== "production") {
 
 const store = createStore(reducer, applyMiddleware(...middlewares))
 
+function ScrollToTop() {
+  window.scrollTo(0, 0)
+  return null
+}
+
 ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL}>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Route component={ScrollToTop}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Route>
   </Router>,
   document.getElementById("root")
 )
